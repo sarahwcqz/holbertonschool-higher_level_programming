abc2a7a9d5fec055ca2b28047cc3f4cf70e273e7
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-02 15:46:03 +0200

    adding last tasks of project if/else

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for char in str:
-		if ord(char) > 96 and ord(char) < 123:
-			print("{}".format(chr(ord(char) - 32)), end="")
-		else:
-			print("{}".format(char), end="")
-	print()
+    for char in str:
+        if ord(char) > 96 and ord(char) < 123:
+            print("{}".format(chr(ord(char) - 32)), end="")
+        else:
+            print("{}".format(char), end="")
