d6283d78bff1116e7544b55cc01b175476e04330
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-02 15:52:39 +0200

    modif on task 1, adding a var to store neg number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit = -(number % -10)
+	pos_number = -number
+	last_digit = pos_number % 10
 else:
-    last_digit = number % 10
+     last_digit = number % 10
 
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".
