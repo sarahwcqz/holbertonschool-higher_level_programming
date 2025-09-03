f52a2c18f6f6e48dfeb61942f4462dc5b6d2b2a8
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-03 14:03:01 +0200

    modif on task 1, changing sign of neg digits

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-	pos_number = -number
-	last_digit = pos_number % 10
+    pos_number = -number
+    last_digit = pos_number % 10
+    last_digit = -last_digit
 else:
      last_digit = number % 10
 
