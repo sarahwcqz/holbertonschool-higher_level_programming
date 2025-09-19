6fddde9dee40969dd7e096ca8cec64b560a00d29
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-19 15:53:03 +0200

    adding task 6 proj classes

---
+++ python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a Square instance.
+        Attributes: size (int): The size of the square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2
+            or not all(isinstance(x, int) and x >= 0 for x in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
+    def size(self):
+        """Gets the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+        Args: value (int): The new size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """Returns the current square area."""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square with the character # to stdout.
+        If size is 0, prints an empty line.
+        """
+        if self.__size == 0:
+            print("")
+            return
+        print("\n" * self.position[1], end="")
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
