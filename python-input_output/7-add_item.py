0392330a710cc4eb3fa8f8595be7c786f72fae17
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-29 15:26:02 +0200

    task 7 scripting a load and write from/ to json, w/ adding if already exists

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to write a script that adds all arguments to a Python list, and then
+save them to a file"""
+
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, "add_item.json")
