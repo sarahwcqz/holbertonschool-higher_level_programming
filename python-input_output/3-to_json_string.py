b1d5859c45f3d8737e4b35c3cda3595087bdbd0b
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-29 14:26:32 +0200

    task 3 ; return a serialized obj

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for converting Python objects to JSON strings."""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON string representation of a Python object."""
+    s_obj = json.dumps(my_obj)
+    return s_obj
