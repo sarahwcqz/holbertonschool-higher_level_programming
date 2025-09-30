3dc279ec407aa129a251d84b0394c98a03c60c47
sarahwcqz <sarah.wacquiez@gmail.com>
2025-09-30 13:36:18 +0200

    modif on 6; pycodestyle

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
     with open(filename, encoding="utf-8") as MyFile:
-            return json.load(MyFile)
+        return json.load(MyFile)
