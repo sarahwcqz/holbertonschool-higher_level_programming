0779d3f9d6d51ceb265a10d19782e47276ad2c98
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-01 15:02:03 +0200

    modif on 2; handling errors

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,7 +8,16 @@ import csv
 
 def convert_csv_to_json(file_name):
     """reads from a csv file and serializes it to a json"""
-    with open(file_name, mode="r", newline="", encoding="utf-8") as csvfile:
-        data = list(csv.DictReader(csvfile))
-    with open("data.json", mode="w", encoding="utf-8") as jsonfile:
-        json.dump(data, jsonfile, indent=4)
+    try:
+        with open(file_name, mode="r", newline="", encoding="utf-8") as csvfile:
+            data = list(csv.DictReader(csvfile))
+        with open("data.json", mode="w", encoding="utf-8") as jsonfile:
+            json.dump(data, jsonfile, indent=4)
+        return True
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+        return False
+    except (OSError, csv.Error, json.JSONDecodeError) as e:
+        print(f"Error: {e}")
+        return False
+
