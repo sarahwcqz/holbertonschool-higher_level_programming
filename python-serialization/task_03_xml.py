4f6e260249e7dbb9b6836403ad8cc3c70030a729
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-04 14:29:52 +0200

    modif on 3; adding simplier indentation to xml file serialized

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -6,36 +6,18 @@ import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
-    """Serialize a dictionary to a simple indented XML file."""
-
     root = ET.Element("data")
 
     for key, value in dictionary.items():
-        element = ET.SubElement(root, key)
-        element.text = str(value)
+        child = ET.SubElement(root, key)
+        child.text = str(value)
 
-    indent_xml(root)
+    ET.indent(root, space="    ")  # indentation automatique
     tree = ET.ElementTree(root)
     tree.write(filename)
 
 
-def indent_xml(elem, level=0):
-    """Add indentation and line breaks for readability."""
-    i = "\n" + "    " * level
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "    "
-        for child in elem:
-            indent_xml(child, level + 1)
-        if not child.tail or not child.tail.strip():
-            child.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
-
-
 def deserialize_from_xml(filename):
-    """Deserialize an XML file into a Python dictionary."""
     tree = ET.parse(filename)
     root = tree.getroot()
     result = {}
