b950e13bed495db0530fb22ea359649bf62c7492
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-04 14:18:34 +0200

    modif on 2; pycodestyle

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,7 +9,8 @@ import csv
 def convert_csv_to_json(file_name):
     """reads from a csv file and serializes it to a json"""
     try:
-        with open(file_name, mode="r", newline="", encoding="utf-8") as csvfile:
+        with open(file_name, mode="r", newline="",
+                  encoding="utf-8") as csvfile:
             data = list(csv.DictReader(csvfile))
         with open("data.json", mode="w", encoding="utf-8") as jsonfile:
             json.dump(data, jsonfile, indent=4)
@@ -20,4 +21,3 @@ def convert_csv_to_json(file_name):
     except (OSError, csv.Error, json.JSONDecodeError) as e:
         print(f"Error: {e}")
         return False
-
