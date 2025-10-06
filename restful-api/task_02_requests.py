32c16638dd1b44030c663ab5900fc10bc317cb98
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-06 19:20:36 +0200

    modif on 2; adding an if status == 200 condition to handle errors

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,24 +4,27 @@ import requests, csv
 
 def fetch_and_print_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(r)
-    data = r.json()
-    for post in data:
-        print(post.get('title'))
+    print(f"Status Code: {r.status_code}")
+    if r.status_code == 200:
+        data = r.json()
+        for post in data:
+            print(post.get('title'))
 
 
 def fetch_and_save_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     data = r.json()
-    post_list = []
-    for post in data:
-        new_post = {
-            'id': post['id'],
-            'title': post['title'],
-            'body': post['body']
-        }
-        post_list.append(new_post)
+    if r.status_code == 200:
+        post_list = []
+        for post in data:
+            new_post = {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+            post_list.append(new_post)
     with open('posts.csv', "w", encoding='utf-8', newline='') as f:
        fieldnames = ['id', 'title', 'body']
        writer = csv.DictWriter(f, fieldnames=fieldnames)
+       writer.writeheader()
        writer.writerows(post_list)
