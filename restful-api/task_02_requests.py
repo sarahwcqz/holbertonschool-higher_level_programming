06d418996e6dedee30fcc05a0ba19b615e0ef3b4
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-06 19:24:09 +0200

    task 2 ; adding doc on fctn + module

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Module to learn consuming and processing data from an API using Python"""
 
-import requests, csv
+import requests
+import csv
+
 
 def fetch_and_print_posts():
+    """fetches some posts from jsonPH and prints titles"""
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
@@ -12,6 +16,7 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
+    """fetches some posts on jsonPH and serializes it to a csv file"""
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     data = r.json()
     if r.status_code == 200:
@@ -24,7 +29,7 @@ def fetch_and_save_posts():
             }
             post_list.append(new_post)
     with open('posts.csv', "w", encoding='utf-8', newline='') as f:
-       fieldnames = ['id', 'title', 'body']
-       writer = csv.DictWriter(f, fieldnames=fieldnames)
-       writer.writeheader()
-       writer.writerows(post_list)
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(post_list)
