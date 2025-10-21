e826bcc06f00032d4a6fb47c1c1322e21d8862a2
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-21 14:59:24 +0200

    removing USE DB because db will be passed as argmt

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- import dump
 -- right outer join
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows RIGHT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- use left outer join
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
