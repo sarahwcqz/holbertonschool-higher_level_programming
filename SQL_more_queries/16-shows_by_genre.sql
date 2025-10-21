c71bbe882dc359fdc587bafaf79e0fcdcf83b0df
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-21 17:09:21 +0200

    changing right join to left join to include the null noted genre of some shows

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- multiples plus left join
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
     ON tv_show_genres.genre_id = tv_genres.id
