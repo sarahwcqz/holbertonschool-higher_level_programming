3cf63a6e32bd3f495b9398bd9fee0ba6c2c3fafe
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-21 14:41:39 +0200

    task 12: left outer join on imported db where genre is null

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- import dump
+-- left outer join w/ null condition
+USE hbtn_0d_tvshows;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
