92d728ef8d02ef21aad3d6fad543cfc5668fb3c4
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-22 11:16:50 +0200

    task 0: connecting python to a db with MySQLbd

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all states from DB
+username, passwd and db name are passed as arguments
+Connecting to a mysql DB
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+        
+    db = MySQLdb.connect(
+            host = 'localhost',
+            port = 3306,
+            user = sys.argv[1],
+            passwd = sys.argv[2],
+            db = sys.argv[3]
+        )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
