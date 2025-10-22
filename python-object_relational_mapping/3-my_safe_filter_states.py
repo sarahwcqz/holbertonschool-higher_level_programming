117b7efd72688578dfeeb3a5ca4e15d43dfa36b4
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-22 14:18:16 +0200

    task 3: using placeholder, secure against sql injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+takes the name of the state as argument
+then displays its info
+-> put a var in the sql query w/ placeholder using placeholder
+    SECURED
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id",
+        (sys.argv[4],)
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
