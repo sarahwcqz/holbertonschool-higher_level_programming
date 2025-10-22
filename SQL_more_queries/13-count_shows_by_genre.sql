857c77189fd762827612c4e50a3b3995ca1425de
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-22 13:17:29 +0200

    task 13: changing as to AS

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list number of show by genre
 -- count/ group by
-SELECT tv_genres.name as genre,
+SELECT tv_genres.name AS genre,
     count(tv_show_genres.show_id) as number_of_shows
 FROM tv_genres JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
