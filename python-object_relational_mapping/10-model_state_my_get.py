ef07b193b0ae78839be07d7ce727a441ea995fd6
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-23 12:23:52 +0200

    task 10: printing id of a state passed in arg

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+List id of state passed as argument
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    spec_name = session.query(State.id).filter(State.name == sys.argv[4])
+
+    for item in spec_name:
+        print(item.id)
+
+    session.close()
