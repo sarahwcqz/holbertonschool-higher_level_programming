9ca8754771fb8915ca240346a4e1e2e9f268a906
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-23 15:00:09 +0200

    task 12: updating a state

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Create a new item in table State
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana_state = State(name='Louisiana')
+    session.add(louisiana_state)
+    session.commit()
+
+    changed_state = (
+        session.query(State)
+        .filter(State.id == 2)
+        .first()
+    )
+
+    if changed_state:
+        changed_state.name = "New Mexico"
+        session.commit()
+
+    session.close()
