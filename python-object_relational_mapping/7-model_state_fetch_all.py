ffde5206c56178cc2f5542325b85b98bb9e6d1ec
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-23 11:28:11 +0200

    task 7: adding order by

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,10 +10,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    all_obj = session.query(State).all()
+    all_obj = session.query(State).order_by(State.id).all()
 
     for row in all_obj:
         print("{}: {}".format(row.id, row.name))
