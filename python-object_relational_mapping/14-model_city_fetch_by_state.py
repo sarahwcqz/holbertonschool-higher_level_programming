88a2e909c241ead07c567df6712e9caafab0b565
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-23 19:55:36 +0200

    task 14: build a city model class and script to list all cities w/ corresponding states

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Create a new item in table State
+"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = (
+        session.query(City, State)
+        .join(State, City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
+
+    for City, State in result:
+        print("{}: ({}) {}".format(City.name, City.id, State.name))
+
+    session.close()
