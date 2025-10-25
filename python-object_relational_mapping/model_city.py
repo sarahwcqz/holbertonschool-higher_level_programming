40e16038f9644c744f2733b7c8b84f1dfe4b8876
sarahwcqz <sarah.wacquiez@gmail.com>
2025-10-25 15:19:22 +0200

    minor modif

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ Defines class City
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import aliased
 from model_state import Base, State
 
 
