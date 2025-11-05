2775e583cf8c8c08451d0ba6978cbdcd300c3e84
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-05 14:54:06 +0100

    semistandard compliant

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-const arg1 = Number(args[2])
+const arg1 = Number(args[2]);
 if (Number.isNaN(arg1)) {
-    console.log('Not a number')
+  console.log('Not a number');
+} else {
+  console.log('My numer: ' + arg1);
 }
-else {
-    console.log('My numer: ' + arg1)
-};
