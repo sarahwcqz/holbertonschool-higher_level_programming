7a972a538e489fa94aa4d31771351f6c53e23eff
sarahwcqz <sarah.wacquiez@gmail.com>
2025-11-05 17:21:03 +0100

    semistandard 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,17 +6,17 @@ let largest = 0;
 let secondLargest = -1;
 
 if (list.length < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    for (let i = 0; i < len; i++) {
-        if (list[i] > largest) {
-            largest = list[i];
-        }
+  for (let i = 0; i < len; i++) {
+    if (list[i] > largest) {
+      largest = list[i];
     }
-    for (let j = 0; j < len; j++) {
-        if (list[j] > secondLargest && list[j] != largest) {
-            secondLargest = list[j];
-        }
+  }
+  for (let j = 0; j < len; j++) {
+    if (list[j] > secondLargest && list[j] !== largest) {
+      secondLargest = list[j];
     }
-    console.log(secondLargest);
+  }
+  console.log(secondLargest);
 }
